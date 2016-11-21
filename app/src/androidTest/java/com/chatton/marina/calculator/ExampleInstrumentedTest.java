ac3b84fd4ff2dd8ec08aa844244e940ff4729b18
Marina Chatton <marina.chatton@gmail.com>
2016-11-21 14:03:23 +0100

    ADD UITests
    added examples of UItests

--- app/src/androidTest/java/com/chatton/marina/calculator/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/chatton/marina/calculator/ExampleInstrumentedTest.java
@@ -2,11 +2,20 @@ package com.chatton.marina.calculator;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -15,7 +24,34 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Test
+    public void shouldDisplayAdditionResultOnClickEqual(){
+        onView(withId(R.id.num5)).perform(click());
+        onView(withId(R.id.operator_plus)).perform(click());
+        onView(withId(R.id.num4)).perform(click());
+        onView(withId(R.id.equal)).perform(click());
+        //check displayed result
+        onView(withId(R.id.display)).check(matches(withText("9")));
+    }
+
+    @Test
+    public void shouldDisplaySubstractionResultOnClickPlus(){
+        onView(withId(R.id.num1)).perform(click());
+        onView(withId(R.id.num5)).perform(click());
+        onView(withId(R.id.operator_min)).perform(click());
+        onView(withId(R.id.num5)).perform(click());
+        onView(withId(R.id.operator_plus)).perform(click());
+        //check displayed result
+        onView(withId(R.id.display)).check(matches(withText("10")));
+    }
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
