0dba6555b5fddaa3ba64a367a9bc1478ed37ae29
Marina Chatton <marina.chatton@gmail.com>
2016-12-02 15:26:21 +0100

    ADD standardFragment

---
+++ app/src/main/java/com/chatton/marina/calculator/StandardFragment.java
@@ -0,0 +1,33 @@
+package com.chatton.marina.calculator;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class StandardFragment extends Fragment {
+
+    public StandardFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_standard, container, false);
+
+        //do the view.findViewById for each button...
+
+        return view;
+    }
+
+}
